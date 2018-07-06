b9c8b39457545e82347a09b05509ce32d3529d6c
AriesKe <liuke@kibey.com>
2018-07-06 21:57:46 +0800

    lot instance

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,6 +4,8 @@ import vueToast from './vtoast.vue'
 let ToastConstructor = Vue.extend(vueToast)
 let instance
 
+let hasShow = true
+
 let init = () => {
   instance = new ToastConstructor({
     el: document.createElement('div')
@@ -12,11 +14,18 @@ let init = () => {
 }
 
 let showToast = (msg, pos='bottom', duration=2000,) => {
+  if (hasShow) {
+    hasShow = false
     init()
     instance.msg_content = msg
     instance.duration = duration
     instance.pos = pos
     instance.show()
+
+    setTimeout(() => {
+      hasShow = true
+    }, duration)
+  }
 }
 
 const finalToast = {
